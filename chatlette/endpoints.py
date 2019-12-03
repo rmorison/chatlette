fb18ab73042aa7d85be4c8ccc09b0ae16abe8ade
rmorison <rmorison@users.noreply.github.com>
2019-12-03 15:45:00 -0800

    add option to for no auth at connect time, for post connect (in band) auth

--- chatlette/endpoints.py
+++ chatlette/endpoints.py
@@ -15,8 +15,14 @@ class ChatWebSocketEndpoint(WebSocketEndpoint):
     redis_url = 'redis://localhost'
     redis_channel_prefix = 'chatlette'
     redis_channel_separator = '/'
-    accept_receive = False
-
+    accept_receive = True
+    require_authenticated = True
+    guest_chatname = 'guest'
+    
+    @property
+    def chatname(self):
+        return self.user.display_name or self.guest_chatname
+    
     async def reader(self, channel: aioredis.pubsub.Channel, websocket: WebSocket) -> None:
         while await channel.wait_message():
             message = await channel.get_json()
@@ -28,11 +34,9 @@ class ChatWebSocketEndpoint(WebSocketEndpoint):
 
     async def on_connect(self, websocket: WebSocket) -> None:
         await super().on_connect(websocket)
-        if not websocket.user.is_authenticated:
-            await websocket.close(status.WS_1008_POLICY_VIOLATION)
-            log.warning(f"closing unauthenticated connection")
+        if self.require_authenticated and not websocket.user.is_authenticated:
+            await self.force_close(websocket)
             return
-        self.accept_receive = True
         self.user = websocket.user
         self.channel_name = self.get_channel_name(websocket)
         self.redis_channel_name = self.redis_channel_separator.join([self.redis_channel_prefix, self.channel_name])
@@ -40,8 +44,13 @@ class ChatWebSocketEndpoint(WebSocketEndpoint):
         self.redis_sub = await aioredis.create_redis(self.redis_url)
         self.redis_sub_channel = (await self.redis_sub.subscribe(self.redis_channel_name))[0]
         asyncio.get_running_loop().create_task(self.reader(self.redis_sub_channel, websocket))
-        log.info(f"chat connect on channel {self.channel_name} by {self.user.username}")
+        log.info(f"chat connect on channel {self.channel_name} by {self.chatname}")
 
+    async def force_close(self, websocket: WebSocket):
+        self.accept_receive = False
+        await websocket.close(status.WS_1008_POLICY_VIOLATION)
+        log.warning(f"force close connection on channel {self.channel_name}")
+        
     async def on_receive(self, websocket: WebSocket, data: typing.Any) -> None:
         if not self.accept_receive:
             log.warning(f"on_receive: {data} but not on_receive not allowed")
@@ -53,7 +62,7 @@ class ChatWebSocketEndpoint(WebSocketEndpoint):
             action_func = getattr(self, f'action_{action}')
         except AttributeError:
             raise NotImplementedError(f"action_{action} not implemented")
-        await action_func(content)
+        await action_func(websocket, content)
             
     async def publish_all(self, response: typing.Any) -> None:
         await self.redis_pub.publish_json(self.redis_channel_name, response)
